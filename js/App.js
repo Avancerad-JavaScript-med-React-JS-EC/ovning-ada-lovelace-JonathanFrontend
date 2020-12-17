0585a48776b9ab831a225602e4dc683ec31ec94e
Jonathan Glad <jonathan.martenssonglad@utb.ecutbildning.se>
2020-12-17 11:42:27 +0100

    klar

---
+++ js/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Img from './Image.js';
+import About from './About.js';
+
+function App(){
+    return (
+        <div>
+            <h1> Ada Lovelace </h1>
+            <Img />
+            <About />
+        </div>
+    )
+}
+
+export default App;
---
+++ js/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Img(){
+    return (
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Ada_Lovelace_portrait.jpg"></img>
+    )
+}
+
+export default Img;
